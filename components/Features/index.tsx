0d0c9469534c2ef87d5371e35c41654abec50314
Dishant Sharma <dishu5570@gmail.com>
2023-01-07 11:18:08 +0530

    landing page v.01 complete

--- components/Features/index.tsx
+++ components/Features/index.tsx
@@ -3,7 +3,7 @@ type Props = {}
 const Features = (props: Props) => {
   return (
     <div>
-      <div className="w-[100vw] flex-col flex items-center">
+      <div id="feature" className="w-[100vw] flex-col flex items-center">
         <div className="w-[90vw] flex flex-col items-center">
           <h2 className="text-center font-[600] md:mt-[4rem] mt-[1rem] text-[4vw] md:text-[2vw]">What are the features of Lazyweb</h2>
           <h2 className="text-center font-[600] text-lightGray opacity-70 mt-[0.5rem] text-[3vw] md:text-[1vw]">Features are highlighted here</h2>
--- components/index.ts
+++ components/index.ts
@@ -3,11 +3,15 @@ import Hero from './Hero';
 import Features from './Features';
 import WhyLazyweb from './WhyLazyweb/index';
 import FeatureDetails from './FeatureDetails/index';
+import Footer from './Footer/index';
+import SEO from './SEO/index';
 
 export { 
   NavBar,
   Hero,
   Features,
   WhyLazyweb,
-  FeatureDetails
+  FeatureDetails,
+  Footer,
+  SEO
  };
